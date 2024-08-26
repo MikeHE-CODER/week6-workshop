7b43df88074ecd8c10e86842bb913c270ee5900e
MikeHE-CODER <a1918791@adelaide.edu.au>
2024-08-26 21:58:57 +0930

    Create warrior.cpp

---
+++ warrior.cpp
@@ -0,0 +1,20 @@
+#include "player.h"
+#include <iostream>
+using namespace std;
+class Warrior : public Player {
+private:
+  string weapon;
+
+public:
+  Warrior(string name, int health, int damage, string weapon) {
+    this->name = name;
+    this->health = health;
+    this->damage = damage;
+    this->weapon = weapon;
+  };
+  void swingWeapon(Player *opponent) {
+    attack(opponent, damage);
+    std::cout << name << " swings their " << weapon << " at "
+              << opponent->getName() << "!\n";
+  };
+};
