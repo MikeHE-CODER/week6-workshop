f814e523b5ee5facaeee8c3709a75f905e8196b2
MikeHE-CODER <a1918791@adelaide.edu.au>
2024-08-26 22:00:13 +0930

    Create wizard.cpp

---
+++ wizard.cpp
@@ -0,0 +1,17 @@
+#include "player.h"
+#include <iostream>
+class Wizard : public Player {
+public:
+  int mana;
+  Wizard(string name, int health, int damage, int mana) {
+    this->name = name;
+    this->health = health;
+    this->damage = damage;
+    this->mana = mana;
+  };
+  void castSpell(Player *opponent) {
+    attack(opponent, mana);
+    std::cout << name << " casts a spell on " << opponent->getName() << " for "
+              << damage << " damage.\n";
+  };
+};
